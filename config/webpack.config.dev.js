5a8319ec89dc73ed6c2cb0265a1b21b950ed6311
Joshua Richey <j@joshuar.com>
2017-04-25 17:08:56 -0700

    added calc for css

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,6 +1,10 @@
 'use strict';
 
+// PostCSS Plugins
 var autoprefixer = require('autoprefixer');
+var calc = require('postcss-calc');
+
+// RCA vars
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
@@ -81,7 +85,7 @@ module.exports = {
       'react-native': 'react-native-web'
     }
   },
-  
+
   module: {
     // First, run the linter.
     // It's important to do this before Babel processes the JS.
@@ -127,7 +131,7 @@ module.exports = {
         include: paths.appSrc,
         loader: 'babel',
         query: {
-          
+
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
           // directory for faster rebuilds.
@@ -161,7 +165,7 @@ module.exports = {
       // Remember to add the new extension(s) to the "url" loader exclusion list.
     ]
   },
-  
+
   // We use PostCSS for autoprefixing only.
   postcss: function() {
     return [
@@ -173,6 +177,7 @@ module.exports = {
           'not ie < 9', // React doesn't support IE8 anyway
         ]
       }),
+      calc
     ];
   },
   plugins: [
